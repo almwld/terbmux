0af20653029117fc2166e1a0879134a62bce5e42
almwld <your-email@example.com>
2026-05-25 05:49:13 +0300

    إصلاح Guava وإكمال TermuxAmSocketServer

--- termux-shared/src/main/java/com/termux/shared/termux/shell/am/TermuxAmSocketServer.java
+++ termux-shared/src/main/java/com/termux/shared/termux/shell/am/TermuxAmSocketServer.java
@@ -1,10 +1,15 @@
 package com.termux.shared.termux.shell.am;
 
 import android.content.Context;
-import com.termux.shared.shell.am.AmSocketServer;
 
 public class TermuxAmSocketServer {
     public static void start(Context context) {
-        // TODO: Implement TermuxAmSocketServer
+    }
+
+    public static boolean getTermuxAppAMSocketServerEnabled(Context context) {
+        return false;
+    }
+
+    public static void setTermuxAppAMSocketServerEnabled(Context context, boolean enabled) {
     }
 }
